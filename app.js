6f1e99dff822121f42514b8d9a046c9c7ab6f18d
husnain.raza <husnain.raza@genetechsolutions.com>
2024-08-09 11:29:46 +0500

    remove some irrelavent file

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/testroutes');
-const routes_products = require('./routes/routes-products');
 const userRoutes = require('./routes/user-routes');
 const cors = require('cors');
 
@@ -14,8 +12,6 @@ app.use(json());
 connectDb();
 app.use(cors()); // Allow all origins
 
-app.use('/', routes);
-app.use('/api/products', routes_products);
 app.use('/api/user',userRoutes);
 
 
